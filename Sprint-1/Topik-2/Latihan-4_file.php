224cecaa49ba282e598f6269d55cf1b148f4c3c7
NurulH123 <hidayatnurul937@gmail.com>
2020-01-10 11:14:55 +0700

    NurulH123

--- Sprint-1/Topik-2/Latihan-4_file.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Menghitung luas persegi
-
-$panjang =5;
-$lebar =8;
-
-echo "Luas persegi tersebut adalah:" ." ". $panjang *= $lebar;
-echo "<br>";
--- Sprint-1/Topik-2/Latihan-5_file.php
+++
@@ -1,9 +0,0 @@
-<?php
-//Menghitung luas lingkaran
-
-$diameter =9;
-
-echo "Luas lingkarannya adalah:" . 3.14*((1/2*$diameter))**2;
-echo "<br>";
-
-echo "Keliling lingkarannya adalah:" . 2*3.14*(1/2*$diameter);
