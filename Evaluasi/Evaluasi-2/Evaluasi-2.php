c73137e2de8271227bbbaa2826ade6fdb14f9ed9
NurulH123 <hidayatnurul937@gmail.com>
2020-01-17 14:43:53 +0700

    NurulH123

--- Evaluasi/Evaluasi-2/Evaluasi-2.php
+++ Evaluasi/Evaluasi-2/Evaluasi-2.php
@@ -1,22 +1,22 @@
 <?php 
 
 function countTriangle($base,$height) {
-    echo "luas = " . 1/2 * $base * $height."\n";
-    echo "keliling =" . (sqrt(2)((1/2*$base)**2+$height**2))+$base+$height; 
+    echo "luas tr= " . 1/2 * $base * $height."\n";
+//     echo "keliling =" . (sqrt(2)((1/2*$base)**2+$height**2))+$base+$height; 
 }
  echo countTriangle(4,8)."\n";
 
 
  function countRectangle($width,$height) {
-    echo "Luas =" . $width+$height."\n";
+    echo "Luas rec=" . $width+$height."\n";
     echo "Keliling =" . 2*($width+$height)."\n";
  }
-echo countRectangle(4,8)."\n";
+echo countRectangle(6,8)."\n";
 
 
  function countCircle($radius,$diameter) {
-    echo "Luas =" . 3.14*(1/2*$diameter)**2;
+    echo "Luas cir=" . 3.14*(1/2*$diameter)**2;
     echo "Keliling =" . 2*3.14*$radius;
  }
- echo countCircle(4,8)."\n";
+ echo countCircle(6,8)."\n";
 ?>
