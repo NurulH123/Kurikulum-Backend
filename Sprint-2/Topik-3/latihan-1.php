9ae0992e04d4b1f2f58f4b37f77f3c61a1dda3d1
NurulH123 <hidayatnurul937@gmail.com>
2020-01-23 08:47:07 +0700

    NurulH123

--- Sprint-2/Topik-3/latihan-1.php
+++ Sprint-2/Topik-3/latihan-1.php
@@ -1,37 +1,71 @@
 <?php
-// function numberDivision(int $devidend, int $divisor) : float {
-//     if($divisor === 0) {
-//         echo 'Pembagi tidak boleh sama dengan 0';
-       
-//     };
 
-//     $quotient = $devidend / $divisor;
-//     return $quotient;
-// }
-
-// numberDivision(5, 3);
-// numberDivision(5, 0);
-
-
-function numberDivision(int $devidend, int $divisor): float {
-    if($divisor === 0) {
-        throw new Exception('Pembagi tidak boleh 0');
+class Hitung
+{ 
+    public function __construct($sejajar1,$sejajar2,$miring,$lebar) {
+        
+        echo "luas trapesium = ". (1/2*($sejajar1+$sejajar2)*$lebar)."\n";
+        echo "keliling trapesium = ".($sejajar1+$sejajar2+$miring+$lebar)."\n\n";
+    }
+    static protected $rumus = 
+    [
+        [
+            'luas persegi panjang','luas lingkaran','luas trapesium'
+        ],
+        [
+            'keliling persegi','keliling lingkaran','keliling trapesium'
+        ],
+        3.14
+    ];
+    
+    public static function persegi($panjang,$tinggi) {
+        $myAngka =self::$rumus;
+        echo $myAngka[0][0] ." ". $panjang*$tinggi."\n";
+        echo $myAngka[1][0]." ".(2*($panjang+$tinggi)."\n\n");
     }
 
-    $quotient = $devidend / $divisor;
-    return $quotient;
+    public static function lingkaran($radius) {
+        $angka =self::$rumus;
+        echo $angka[0][1] ." ". ($angka[2]*($radius)**2) ;
+        echo "\n";
+        echo $angka[1][1] ." ". (2*$angka[2]*$radius);
+    }
 }
+        
+function warning(int $acces, int $trouble): float {
+    if($trouble < 0) {
+        throw new Exception('Angka tidak boleh kurang dari 0');
+            echo "\n";
+         $masuk =$acces * $trouble;
+         return $masuk;
+    }
+}
+    echo "Masukkan nilai panjang :";
+    $panjang = (int) fgets(STDIN);
+    echo "\n"."Masukkan nilai lebar :";
+    $tinggi = (int) fgets(STDIN);
 
-$result =null;
+    $result =null;
 
 try{
-    $result = numberDivision(5,0);
-}catch (Exception $e) {
-    echo "Error pada".__FILE__."baris ke : ".$e->getline()."\n";
-    echo $e->getMessage();
+    $result = warning($panjang,$tinggi);
+}catch(Exception $nol) {
+    echo "Error".__FILE__."baris ke-".$nol->getline()."\n";
+    echo $nol->getMessage();
+    $jalan = New Hitung($panjang,$lebar,4,3);
 }
 
-echo $result."\n";
 
+
+$radius = 4;
+$sejajar1 = 5;
+$sejajar2 = 10;
+$miring = 3;
+$panjang = 7;
+$tinggi = 4;
+
+new Hitung($sejajar1,$sejajar2,$miring,$tinggi);
+Hitung::persegi($panjang,$tinggi);
+Hitung::lingkaran($radius);
 
 ?>
